0ee8d326995fbeebcab236af75c76ff1702690ff
Magshimim_AI <kwdaenoa1@gmail.com>
2024-11-07 22:17:09 +0200

    final first exercise

--- first_exercise/first.c
+++ first_exercise/first.c
@@ -7,13 +7,16 @@ int main(void)
     int countS = 0;
     double e1, e2, e3;
     printf("Please enter first side to a traingle: \n");
-    scanf("%f", &e1);
+    scanf(" %lf", &e1);
     printf("Please enter second side to a traingle: \n");
-    scanf("%f", &e2);
+    scanf(" %lf", &e2);
     printf("Please enter third side to a traingle: \n");
-    scanf("%f", &e3);
-    while(e1 != 0 && e2 != 0 && e3 != 0)
+    scanf(" %lf", &e3);
+    while(e1 != 0.0 && e2 != 0.0 && e3 != 0.0)
     {
+        printf("e1:%lf", e1);
+        printf(" e2:%lf", e2);
+        printf(" e3:%lf", e3);
         if(e1 + e2 > e3 && e1 + e3 > e2 && e2 + e3 > e1)
         {
             if(e1*e1 + e2*e2 == e3*e3)
@@ -23,20 +26,20 @@ int main(void)
                 countS += 1;
                 printf("The Sides you gave make a right traingle and its area is: %.2f\n", area);
             }
-            if(e3*e3 + e2*e2 == e1*e1)
+            else if(e3*e3 + e2*e2 == e1*e1)
             {
                 area = (e3 * e2)/2;
                 sumS += area;
                 countS += 1;
                 printf("The Sides you gave make a right traingle and its area is: %.2f\n", area);
             }
-            if(e1*e1 + e3*e3 == e2*e2)
+            else if(e1*e1 + e3*e3 == e2*e2)
             {
                 area = (e1 * e3)/2;
                 sumS += area;
                 countS += 1;
                 printf("The Sides you gave make a right traingle and its area is: %.2f\n", area);
-            }
+            }  
             else
             {
                 printf("The sides you gave make a traingle\n");
@@ -47,13 +50,13 @@ int main(void)
             printf("the 3 numbers cannot be a traingle\n");
         }
         printf("Please enter first side to a traingle: \n");
-        scanf("%f", &e1);
+        scanf("%lf", &e1);
         printf("Please enter second side to a traingle: \n");
-        scanf("%f", &e2);
+        scanf("%lf", &e2);
         printf("Please enter third side to a traingle: \n");
-        scanf("%f", &e3);
+        scanf("%lf", &e3);
     }
-    printf("the sum of the traingles %f",sumS);
-    printf(" and the average area of the traingles is %f",sumS/countS);
+    printf("the sum of the traingles %lf",sumS);
+    printf(" and the average area of the traingles is %lf",sumS/countS);
     return 0;
 }
